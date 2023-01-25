37c010f6e33881428089122c95b2674cc47b59c7
Zero <node.zerocool@gmail.com>
2023-01-24 22:46:44 -0600

    hours and seconds, move to minutes and seconds

--- scripts.js
+++ scripts.js
@@ -1,3 +1,26 @@
+// function countDownTimer() {
+//     // Get the current date and time
+//     var currentDate = new Date();
+    
+//     // Set the target date and time to 2pm Pacific Time
+//     var targetDate = new Date();
+//     targetDate.setUTCHours(21); 
+//     targetDate.setUTCMinutes(0);
+//     targetDate.setUTCSeconds(0);
+    
+//     // Calculate the remaining time
+//     var remainingTime = targetDate - currentDate;
+    
+//     // Convert the remaining time to hours and seconds
+//     var hours = Math.floor(remainingTime / (1000 * 60 * 60));
+//     var seconds = Math.floor((remainingTime % (1000 * 60 * 60)) / 1000);
+    
+//     // Display the remaining time in a div
+//     var timerDiv = document.getElementById("timer");
+//     timerDiv.innerHTML = "Time remaining: " + hours + " hours and " + seconds + " seconds";
+//   }
+// countDownTimer();
+
 function countDownTimer() {
     // Get the current date and time
     var currentDate = new Date();
@@ -17,6 +40,11 @@ function countDownTimer() {
     
     // Display the remaining time in a div
     var timerDiv = document.getElementById("timer");
-    timerDiv.innerHTML = "Time remaining: " + hours + " hours and " + seconds + " seconds";
-  }
-countDownTimer();
+    if(remainingTime<=0){
+      timerDiv.innerHTML = "Time is up";
+    }else{
+      timerDiv.innerHTML = "Time remaining: " + hours + " hours and " + seconds + " seconds";
+    }
+}
+
+setInterval(countDownTimer, 1000);
